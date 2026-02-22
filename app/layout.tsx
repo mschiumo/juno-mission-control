99ea99bfdbf02c5f665fc10d81f16ba35daaa517
Juno <juno@keepliving.com>
2026-02-22 06:08:39 +0000

    feat: update favicon to orange dot with J logo
    - Add new favicon.svg with orange dot (#FF6B35) and white J letter
    - Update layout.tsx metadata to use the new favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,11 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Juno Mission Control",
   description: "Personal dashboard for mission control",
+  icons: {
+    icon: "/favicon.svg",
+    shortcut: "/favicon.svg",
+    apple: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({
