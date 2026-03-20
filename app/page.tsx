cc98b97e21ee59638b045987871184cf63f105da
Juno <juno@keepliving.com>
2026-03-20 22:43:54 +0000

    feat: Remove Juno status badge/widget
    - Delete JunoWidget component
    - Remove imports and usages from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,6 @@ import GapScannerCard from "@/components/GapScannerCard";
 import NewsScreenerCard from "@/components/NewsScreenerCard";
 import ProjectsCard from "@/components/ProjectsCard";
 import GoalsCard from "@/components/GoalsCard";
-import JunoWidget from "@/components/JunoWidget";
 import LiveClock from "@/components/LiveClock";
 import NotificationsBell from "@/components/NotificationsBell";
 import MotivationalBanner from "@/components/MotivationalBanner";
@@ -96,7 +95,6 @@ function DashboardContent() {
               {/* Desktop Widgets */}
               <div className="hidden md:flex items-center gap-4">
                 <NotificationsBell />
-                <JunoWidget />
                 <LiveClock />
               </div>
 
@@ -138,7 +136,6 @@ function DashboardContent() {
               </div>
               <div className="mt-4 pt-4 border-t border-[#30363d] flex items-center justify-between">
                 <NotificationsBell />
-                <JunoWidget />
                 <LiveClock />
               </div>
             </div>
